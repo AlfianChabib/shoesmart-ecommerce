c8dead620c4c484d72f4ea624a2bc66a73776f4e
AlfianChabib <alfianchabib109@gmail.com>
2024-06-05 20:40:11 +0700

    feat: update 3

--- apps/web/src/hooks/alert-message.ts
+++ apps/web/src/hooks/alert-message.ts
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 export const useAlertMessage = (): {
   alertMessage: AlertMessageProps | undefined;
+
   setAlertMessage: React.Dispatch<React.SetStateAction<AlertMessageProps | undefined>>;
 } => {
   const [alertMessage, setAlertMessage] = useState<AlertMessageProps | undefined>(undefined);
